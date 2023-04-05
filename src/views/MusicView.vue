95176595d1eeee9d53600c88ec9b97215afe189c
Dylan Hurtado López <dylahurtado43@gmail.com>
2023-04-06 01:50:13 +0200

    Adding videos to music

--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -7,13 +7,9 @@ export default {
     methods: {
       controlVideo(event){
         if(event.type ==="mouseenter"){
-          event.target.childNodes[0].setAttribute("autoplay","")
-          event.target.childNodes[0].setAttribute("loop","")
-          event.target.childNodes[0].setAttribute("muted","")
+          event.target.childNodes[0].removeAttribute("style")
         }else{
-          event.target.childNodes[0].removeAttribute("autoplay")
-          event.target.childNodes[0].removeAttribute("loop")
-          event.target.childNodes[0].removeAttribute("muted")
+          event.target.childNodes[0].setAttribute("style","filter: grayscale(1);")
         }
       }
     },
@@ -25,33 +21,55 @@ export default {
 
     <div class="repertorio">
   <div class="card" @mouseenter="controlVideo($event)"  @mouseleave="controlVideo($event)">
-    <video src="" autoplay loop muted ></video>
+    <img src="../assets/videos/SeekAndDestroyMetallica.gif" class="image"  style="filter:grayscale(1)"  />
     <div class="info">
-      <h3 class="minititle">Nombre de la canción 1</h3>
-      <p>Autor 1</p>
+      <h3 class="minititle">Seek And Destroy</h3>
+      <p>Metallica</p>
     </div>
   </div>
 
   <div class="card" @mouseenter="controlVideo($event)"  @mouseleave="controlVideo($event)" >
-    <video src="" loop muted ></video>
+    <img src="../assets/videos/HeavenOnFireKISS.gif" class="image"  style="filter:grayscale(1)"  />
     <div class="info">
-      <h3 class="minititle">Nombre de la canción 2</h3>
-      <p>Autor 2</p>
+      <h3 class="minititle">Heaven's On Fire</h3>
+      <p>KISS</p>
     </div>
   </div>
 
   <div class="card" @mouseenter="controlVideo($event)"  @mouseleave="controlVideo($event)" >
-    <video src="" autoplay loop muted ></video>
+    <img src="../assets/videos/cantStopRHCP.gif" class="image" style="filter:grayscale(1)" />
     <div class="info">
-      <h3 class="minititle">Nombre de la canción 3</h3>
-      <p>Autor 3</p>
+      <h3 class="minititle">Can't stop</h3>
+      <p>Red Hot Chili Peppers</p>
     </div>
   </div>
+  <div class="card" @mouseenter="controlVideo($event)"  @mouseleave="controlVideo($event)" >
+    <img src="../assets/videos/SweatingBulletsMegadeth.gif" class="image" style="filter:grayscale(1)" />
+    <div class="info">
+      <h3 class="minititle">Sweating Bullets</h3>
+      <p>Megadeth</p>
+    </div>
+  </div>
+  <div class="card" @mouseenter="controlVideo($event)"  @mouseleave="controlVideo($event)" >
+    <img src="../assets/videos/HolyDiverDio.gif" class="image" style="filter:grayscale(1)" />
+    <div class="info">
+      <h3 class="minititle">Holy Diver</h3>
+      <p>DIO</p>
+    </div>
+  </div>
+  <div class="card" @mouseenter="controlVideo($event)"  @mouseleave="controlVideo($event)" >
+    <img src="../assets/videos/SpeedKingDeepPurple.gif" class="image" style="filter:grayscale(1)" />
+    <div class="info">
+      <h3 class="minititle">Speed King</h3>
+      <p>Deep purple</p>
+    </div>
+  </div>
+
 </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 
   .music {
     min-height: 100vh;
@@ -64,6 +82,7 @@ export default {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
+    transition: all .3s;
   }
   .card{
     display: flex;
@@ -71,10 +90,16 @@ export default {
     margin: 20px;
     overflow: hidden;
     border-radius: 10px;
+    border:  1px solid var(--vt-c-indigo);
+  }
+
+  .card:hover{
+   scale: 1.1;
   }
   .image{
-    width: 100%;
-    height: 240px;
+    max-width: 25vh;
+    min-width: 25vh;
+    height: 20vh;
     object-fit: cover;
   }
   .info{
@@ -87,5 +112,6 @@ export default {
   }
   .minititle{
     font-weight: bold;
+    color:greenyellow;
   }
 </style>
